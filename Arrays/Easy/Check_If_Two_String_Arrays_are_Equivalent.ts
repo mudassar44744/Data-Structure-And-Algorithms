7aebc1666ea899c117663602bc1c947e227c05e9
mudassar44744 <mudassarmustafai@outlook.com>
2024-01-30 11:44:22 +0500

    string equal

---
+++ Arrays/Easy/Check_If_Two_String_Arrays_are_Equivalent.ts
@@ -0,0 +1,9 @@
+// Leetcode: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+var arrayStringsAreEqual = (word1: Array<string>, word2: Array<string>): boolean => word1.join("") === word2.join("");
+
+// var arrayStringsAreEqual = function(word1, word2) {
+//     word1 = word1.join('');
+//     word2 = word2.join('');
+//     return word1 == word2 ? true : false;
+// };
