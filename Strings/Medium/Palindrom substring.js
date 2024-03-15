0c9461a8853f934d1552b64236b4fc7f45e1762e
mudassar44744 <mudassarmustafai@outlook.com>
2024-03-15 21:22:20 +0500

    palindrom substring

--- Strings/Medium/Palindrom substring.js
+++ Strings/Medium/Palindrom substring.js
@@ -1,11 +1,10 @@
 //LeetCode : https://leetcode.com/problems/palindromic-substrings/description/
 
-
 /**
  * @param {string} s
  * @return {number}
  */
-var countSubstrings = function(s) {
+var countSubstrings1 = function(s) {
     let res = 0;
    
     for (let i = 0; i < s.length; i++) {
@@ -29,4 +28,72 @@ var countSubstrings = function(s) {
     return res
     
 }
+
+//https://www.youtube.com/watch?v=J5P4OBhFKq0&ab_channel=CSIITIAN
+
+var countSubstrings2 = function(s) {
+    let totalCount = 0;
+    for (let i = 0; i < s.length; i++) {
+        // Count palindromes with odd length
+        totalCount += extendPalindrome(s, i, i);
+        // Count palindromes with even length
+        totalCount += extendPalindrome(s, i, i + 1);
+    }
+    return totalCount;
+};
+
+const extendPalindrome = (s, left, right) => {
+    let count = 0;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+        count++;
+        left--;
+        right++;
+    }
+    return count;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let count = 0;
+    for(let i = 0; i < s.length; i++) {
+       for(let j = i; j < s.length; j++) {
+        if(isPalindrom(i,j))  count++;
+       }
+    }
+    return count;
+
+    function isPalindrom(start, end) {
+        while(start <= end) {
+            if(s[start] !== s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+  
+}
+
+/*
+substring 
+aaa
+
+a, a, a, aa, aa, aaa
+
+Brute force approach 
+time complexity n3
+time complexity o1
+*/
+
+console.log(
+    countSubstrings('aaa')
+)
+
+console.log(
+    countSubstrings('abc')
+)
 
